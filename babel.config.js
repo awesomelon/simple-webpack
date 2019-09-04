1c2cb85ce4b993a16e3dfe34aa53a7fe02eb6c33
awesomelon <awesomelon@naver.com>
2019-09-04 10:50:52 +0900

    fix babel.config.js  presets, plugins

--- babel.config.js
+++ babel.config.js
@@ -4,29 +4,32 @@ function isBabelRegister(caller) {
 
 module.exports = function(api) {
     api.caller(isBabelRegister);
+    api.cache(false);
+
+    const presets = [
+        [
+            '@babel/preset-env',
+            {
+                useBuiltIns: 'usage',
+                corejs: { version: 3, proposals: true },
+                targets: {
+                    browsers: ['last 2 version', 'ie >= 10']
+                },
+                loose: true,
+                debug: true
+            }
+        ]
+    ].filter(Boolean);
+
+    const plugins = [
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-transform-runtime'
+    ].filter(Boolean);
 
     return {
-        presets: [
-            [
-                '@babel/preset-env',
-                {
-                    useBuiltIns: 'usage',
-                    corejs: { version: 3, proposals: true },
-                    targets: {
-                        browsers: ['last 2 version', 'ie >= 10']
-                    },
-                    loose: true,
-                    debug: true
-                }
-            ]
-        ],
-        plugins: [
-            [
-                '@babel/plugin-transform-runtime',
-                {
-                    corejs: 3
-                }
-            ]
-        ]
+        presets,
+        plugins
     };
 };
