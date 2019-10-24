64fd4fc442a34c5e58b204abf8ae6526e11d67d1
awesomelon <awesomelon@naver.com>
2019-10-24 17:10:08 +0900

    webpack.config dev 수정

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -75,25 +75,7 @@ module.exports = {
             }
         ]
     },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                    test: /[\\/]node_modules[\\/]/,
-                    // cacheGroupKey here is `commons` as the key of the cacheGroup
-                    name(module, chunks, cacheGroupKey) {
-                        const moduleFileName = module
-                            .identifier()
-                            .split('/')
-                            .reduceRight(item => item);
-                        const allChunksNames = chunks.map(item => item.name).join('~');
-                        return `${cacheGroupKey}-${allChunksNames}-${moduleFileName}`;
-                    },
-                    chunks: 'all'
-                }
-            }
-        }
-    },
+
     plugins: [
         new HtmlWebPackPlugin({
             title: '개발용',
