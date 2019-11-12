c280bdc8e96bd2aa435f2b9421078571b4846c66
awesomelon <awesomelon@naver.com>
2019-11-12 11:54:41 +0900

    prettierrc 추가

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    singleQuote: true,
+    printWidth: 100,
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    jsxBracketSameLine: true,
+    jsxSingleQuote: true
+};
