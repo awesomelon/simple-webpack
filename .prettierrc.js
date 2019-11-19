70e3f8607e24444fd858b5380c1d18e217ad3c11
awesomelon <awesomelon@naver.com>
2019-11-19 15:18:08 +0900

    prettierrc.js 삭제

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    singleQuote: true,
-    printWidth: 100,
-    tabWidth: 4,
-    semi: true,
-    singleQuote: true,
-    jsxBracketSameLine: true,
-    jsxSingleQuote: true
-};
